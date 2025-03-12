67167061691983677bafeeb93333ca5ca356c648
Phoenix69 <rkii939b@gmail.com>
2025-03-12 23:01:03 +0100

    header included

--- assistant.h
+++ assistant.h
@@ -1,10 +1,28 @@
+// Copyright (C) 2017 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
 #ifndef ASSISTANT_H
 #define ASSISTANT_H
 
-class assistant
+#include <QCoreApplication>
+#include <QProcess>
+#include <QScopedPointer>
+#include <QString>
+
+class Assistant
 {
+    Q_DECLARE_TR_FUNCTIONS(Assistant)
+
 public:
-    assistant();
+    ~Assistant();
+    void showDocumentation(const QString &file);
+
+private:
+    bool startAssistant();
+    void showError(const QString &message);
+    void finished(int exitCode, QProcess::ExitStatus status);
+
+    QScopedPointer<QProcess> m_process;
 };
 
-#endif // ASSISTANT_H
+#endif
