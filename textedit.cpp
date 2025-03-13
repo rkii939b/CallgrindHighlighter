a738c718a53cf959d35bec01b1c46c264c98bec3
Phoenix69 <rkii939b@gmail.com>
2025-03-13 14:56:05 +0100

    added and updated textedit

--- textedit.cpp
+++ textedit.cpp
@@ -1,3 +1,49 @@
 #include "textedit.h"
+#include <QFile>
+#include <QFileInfo>
+#include <QDebug>
 
-textedit::textedit() {}
+TextEdit::TextEdit(QWidget *parent)
+    : QTextEdit(parent), m_highlighter(nullptr)
+{
+    setReadOnly(true);
+}
+
+void TextEdit::setContents(const QString &fileName)
+{
+    // Clear existing content and highlighter
+    clear();
+    delete m_highlighter;
+    m_highlighter = nullptr;
+
+    QFileInfo fi(fileName);
+    srcUrl = QUrl::fromLocalFile(fi.absoluteFilePath());
+
+    if (fileName.endsWith(".callgrind", Qt::CaseInsensitive) ||
+        fileName.contains("callgrind.out")) {
+        qDebug() << "Initializing Callgrind highlighter for:" << fileName;
+        m_highlighter = new CallgrindSyntaxHighlighter(document());
+    }
+
+    QFile file(fileName);
+    if (file.open(QIODevice::ReadOnly)) {
+        QString content = QString::fromUtf8(file.readAll());
+        if (fileName.endsWith(".html")) {
+            setHtml(content);
+        } else {
+            setPlainText(content);
+        }
+    }
+
+    emit fileNameChanged(fileName);
+}
+
+QVariant TextEdit::loadResource(int type, const QUrl &name)
+{
+    if (type == QTextDocument::ImageResource) {
+        QFile file(srcUrl.resolved(name).toLocalFile());
+        if (file.open(QIODevice::ReadOnly))
+            return file.readAll();
+    }
+    return QTextEdit::loadResource(type, name);
+}
--- textedit.h
+++ textedit.h
@@ -1,10 +1,24 @@
 #ifndef TEXTEDIT_H
 #define TEXTEDIT_H
 
-class textedit
+#include <QTextEdit>
+#include "callgrindhighlighter.h"
+
+class TextEdit : public QTextEdit
 {
+    Q_OBJECT
 public:
-    textedit();
+    explicit TextEdit(QWidget *parent = nullptr);
+    void setContents(const QString &fileName);
+
+signals:
+    void fileNameChanged(const QString &fileName);
+
+private:
+    CallgrindSyntaxHighlighter *m_highlighter = nullptr;
+    QUrl srcUrl;
+
+    QVariant loadResource(int type, const QUrl &name) override;
 };
 
 #endif // TEXTEDIT_H
