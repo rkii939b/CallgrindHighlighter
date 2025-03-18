d7a39cbf270d309056fd172af06a03668587745b
Phoenix69 <rkii939b@gmail.com>
2025-03-19 00:24:40 +0100

    Build folder containing callgrind files

---
+++ build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_simpletextviewer_FILE E:/Projects/simpleTextViewer/CallgrindHighlighter/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/simpletextviewer.exe)
+set(__QT_DEPLOY_TARGET_simpletextviewer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_simpletextviewer_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
