a4e2deec950621f7cf64482eef458a04dd64a875
Phoenix69 <rkii939b@gmail.com>
2025-03-19 21:03:09 +0100

    Copyright bz QT code of assistant.h

--- assistant.h
+++ assistant.h
@@ -1,5 +1,6 @@
-// Copyright (C) 2017 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// Copyright (C) 2017 The Qt Company Ltd.
+
 
 #ifndef ASSISTANT_H
 #define ASSISTANT_H
